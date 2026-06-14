9304ba814aeda6a12932b4f696d0373592a1d2be
uvaxn <xpanic3z@gmail.com>
2026-06-13 20:23:11 -0700

    airTimeScalar changed to 7.629

--- Robot2026/main/src/main/java/frc/robot/Vars.java
+++ Robot2026/main/src/main/java/frc/robot/Vars.java
@@ -7,9 +7,9 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 public class Vars {
     
     public static final double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-    public static final double AlignToHubP = 1.35;
-    public static final double AlignToHubI = 0.07;
-    public static final double AlignToHubD = 0.1;
+    public static final double AlignToHubP = 25.0;
+    public static final double AlignToHubI = 5.0;
+    public static final double AlignToHubD = 0.5;
     // controller stoff
     public static final SlewRateLimiter xLimiter   = new SlewRateLimiter(7.0, -7.0, 0);
     public static final SlewRateLimiter yLimiter   = new SlewRateLimiter(7.0, -7.0, 0);
@@ -23,7 +23,7 @@ public class Vars {
     public static double SHOOTER_SPEED = 0.8;
     public static double FEED_SPEED = 0.8;
     //
-    public static final double airTimeScalarSeconds = 5.629; // higher number = less predicition
+    public static final double airTimeScalarSeconds = 7.629; // higher number = less predicition
     public static final double MaxAngularRate = 1;
     public static final double maxYawRateForVision = 0;
 }
