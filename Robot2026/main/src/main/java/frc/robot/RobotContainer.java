2ebb3eb7285bb2a7cd5951fa4d719472b9d0904b
uvaxn <xpanic3z@gmail.com>
2026-06-19 13:04:23 -0700

    comp right auto finalizing

--- Robot2026/main/src/main/java/frc/robot/RobotContainer.java
+++ Robot2026/main/src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,6 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
