773e47e2b8c4de663a7c79ef9a9ab4b5918c3c1f
uvaxn <xpanic3z@gmail.com>
2026-06-29 12:11:37 -0700

    vars

--- src/main/java/frc/robot/Vars.java
+++ src/main/java/frc/robot/Vars.java
@@ -6,20 +6,20 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 
 public class Vars {
     
-    public static final double MaxSpeed = 0.6 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-    public static final double AlignToHubP = 25.0;
+    public static final double MaxSpeed = 0.7 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+    public static final double AlignToHubP = 45.0;
     public static final double AlignToHubI = 5.0;
     public static final double AlignToHubD = 0.0;
 
-    public static final double AlignToAllianceWallP = 35.0;
-    public static final double AlignToAllianceWallI = 10.0;
+    public static final double AlignToAllianceWallP = 55.0;
+    public static final double AlignToAllianceWallI = 5.0;
     public static final double AlignToAllianceWallD = 0.0;
     // controller stoff
 
     public final static SlewRateLimiter xLimiter = new SlewRateLimiter(1);
     public final static SlewRateLimiter yLimiter = new SlewRateLimiter(1);
     // no more controller stuph 
-    public static double FEED_SPEED = 0.8; // in percentage (0.8 == 80%)
+    public static double FEED_SPEED = 0.6; // in percentage (0.8 == 80%)
     public static double SHOOTER_SPEED = 30;
     //
     public static final double airTimeScalarSeconds = 1;
