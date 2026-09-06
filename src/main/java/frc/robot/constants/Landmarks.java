0c51a266eb07e95840ffc1902892e8f47b6f30c8
uvaxn <xpanic3z@gmail.com>
2026-09-06 11:38:07 -0700

    added landmarks (but louder)

--- src/main/java/frc/robot/constants/Landmarks.java
+++ src/main/java/frc/robot/constants/Landmarks.java
@@ -5,12 +5,9 @@ import edu.wpi.first.math.geometry.Translation2d;
 public final class Landmarks {
 
     private Landmarks() {}
-
-    // --- Your own measured hub positions ---
     public static final Translation2d blueHubPosition = Constants.blueHubPosition;
     public static final Translation2d redHubPosition = Constants.redHubPosition;
 
-    /** Alliance-aware hub position. Same behavior as {@link Constants#getTeamHubTranslation()}. */
     public static Translation2d hubPosition() {
         return Constants.getTeamHubTranslation();
     }
